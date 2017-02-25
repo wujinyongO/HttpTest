2208df6b014ac8ee92bcd253cfbdb0d2c1572914
cielwu <cielwu>
2017-02-25 23:16:31 +0800

    Mutidownload

--- app/src/main/java/com/example/administrator/httptest_2/MainActivity.java
+++ app/src/main/java/com/example/administrator/httptest_2/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
     }
 
     public void onClick(View v){
---
+++ app/src/main/java/com/example/administrator/httptest_2/MutiThreadDownload/MutiThreadDownloadActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.httptest_2.MutiThreadDownload;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cielwu on 2017/2/25.
+ */
+
+public class MutiThreadDownloadActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
